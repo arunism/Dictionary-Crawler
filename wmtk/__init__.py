28f96247c7d30f834fd3fddd691c63b3ef9fee40
arunism <thearunism@gmail.com>
2022-08-12 00:35:34 +0545

    small updates

--- wmtk/__init__.py
+++ wmtk/__init__.py
@@ -1,1 +1,5 @@
 __version__ = '0.1.0'
+
+from wmtk.google import Google
+
+__all__ = ['Google']
---
+++ wmtk/config.py
@@ -0,0 +1,4 @@
+# GOOGLE
+GOOGLE_CUSTOM_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1?'
+GOOGLE_API_KEY = 'AIzaSyC9dTyRuG60TQbVozcdYkPmuQCkogDKqVU'
+GOOGLE_SEARCH_ENGINE_ID = 'f1b22ac184bf54482'
